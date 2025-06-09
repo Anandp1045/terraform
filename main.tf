9fe0b25dd5f7aa0b5515537c7bd7044e40c7c5c4
Anandp1045 <vijayanand1045@gmail.com>
2025-06-09 09:19:35 +0000

    first push

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.95.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-southeast-1"
+}
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Terra-VPC"
+  }
+}
